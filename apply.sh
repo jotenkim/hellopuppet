8bbb4c6aab85193c854c6a05b6f9201c25f24472
Kim <wertyy@gmail.com>
2017-04-12 14:14:00 +0000

    modified apply.sh

--- apply.sh
+++ apply.sh
@@ -1,2 +1,2 @@
-sudo puppet apply --modulepath puppet/modules/ -e 'class {bashaliashistory:}'
-sudo puppet apply --modulepath puppet/modules/ -e 'class {ssh:}'
+sudo puppet apply -e 'class {bashaliashistory:}'
+sudo puppet apply -e 'class {ssh:}'
